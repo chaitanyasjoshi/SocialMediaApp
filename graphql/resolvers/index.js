4fd531408b6a62392bc06445f60e42feff1d095e
Deadpool812 <chaitanya9joshi@gmail.com>
2021-06-05 14:33:39 +0530

    Allow user to create and delete  a comment

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,5 +1,6 @@
 const postResolvers = require('./posts');
 const userResolvers = require('./users');
+const commentResolvers = require('./comments');
 
 module.exports = {
   Query: {
@@ -9,5 +10,6 @@ module.exports = {
   Mutation: {
     ...postResolvers.Mutation,
     ...userResolvers.Mutation,
+    ...commentResolvers.Mutation,
   },
 };
