4ead01f851d56fcba62e2e0ed189ba2d1cfe48df
Deadpool812 <chaitanya9joshi@gmail.com>
2021-06-06 09:50:16 +0530

    Add likes and comments count

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -4,6 +4,10 @@ const commentResolvers = require('./comments');
 const likeResolvers = require('./likes');
 
 module.exports = {
+  Post: {
+    likesCount: async (parent) => parent.likes.length,
+    commentsCount: async (parent) => parent.comments.length,
+  },
   Query: {
     ...postResolvers.Query,
     ...userResolvers.Query,
