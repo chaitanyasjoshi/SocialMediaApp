081cde5eca3b6d95cd84494648ec67b0c06c3dd1
Deadpool812 <chaitanya9joshi@gmail.com>
2021-06-07 11:57:00 +0530

    Create post from client

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -1,15 +1,65 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import React from 'react';
-import { Form } from 'semantic-ui-react';
+import { Button, Form } from 'semantic-ui-react';
 
 import { useForm } from '../utils/hooks';
 
 export default function PostForm() {
+  const { values, onChange, onSubmit } = useForm(createPostCallback, {
+    body: '',
+  });
+
+  const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
+    variables: values,
+    update(_, result) {
+      console.log(result);
+      values.body = '';
+    },
+  });
+
+  function createPostCallback() {
+    createPost();
+  }
+
   return (
     <Form onSubmit={onSubmit}>
       <h2>Create Post:</h2>
       <Form.Field>
-        <Form.Input placeholder='Hi World!' />
+        <Form.Input
+          placeholder='Hi World!'
+          name='body'
+          onChange={onChange}
+          value={values.body}
+        />
+        <Button type='submit' color='teal'>
+          Post
+        </Button>
       </Form.Field>
     </Form>
   );
 }
+
+const CREATE_POST_MUTATION = gql`
+  mutation createPost($body: String!) {
+    createPost(body: $body) {
+      id
+      body
+      createdAt
+      username
+      likes {
+        id
+        username
+        createdAt
+      }
+      likesCount
+      comments {
+        id
+        body
+        username
+        createdAt
+      }
+      commentsCount
+    }
+  }
+`;
