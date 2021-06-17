7b6cd9da1eac9fc036ac8803dee85066ca43104e
Deadpool812 <chaitanya9joshi@gmail.com>
2021-06-17 10:48:35 +0530

    Fix port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { MONGO_URL } = require('./config');
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
   typeDefs,
